31c646d7909491a2fbed09e54d1b355ebe4cd8eb
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2024-11-01 01:57:34 +0000

    staging Fix route naming conflict

--- application/routes/api.php
+++ application/routes/api.php
@@ -15,25 +15,25 @@ use App\Http\Controllers\API\APICustomerCategoriesController;
 Route::prefix('v1')->middleware('auth:sanctum')->group(static function () {
 
     // Customers
-    Route::resource('customers', APICustomersController::class)->except(['create', 'edit', 'destroy']);
-    Route::resource('customers.emails', APICustomerEmailsController::class)->except(['create', 'edit', 'destroy'])->scoped();
-    Route::resource('customers.phones', APICustomerPhonesController::class)->except(['create', 'edit', 'destroy'])->scoped();
-    Route::resource('customers.addresses', APICustomerAddressesController::class)->except(['create', 'edit', 'destroy'])->scoped();
-    Route::resource('customer-categories', APICustomerCategoriesController::class)->except(['create', 'edit', 'destroy']);
+    Route::resource('customers', APICustomersController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy']);
+    Route::resource('customers.emails', APICustomerEmailsController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy'])->scoped();
+    Route::resource('customers.phones', APICustomerPhonesController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy'])->scoped();
+    Route::resource('customers.addresses', APICustomerAddressesController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy'])->scoped();
+    Route::resource('customer-categories', APICustomerCategoriesController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy']);
     Route::put('customer-categories-sync', [APICustomerCategoriesController::class, 'sync']);
 
     // Products
-    Route::resource('products', APIProductsController::class)->except(['create', 'edit', 'destroy']);
-    Route::resource('product-categories', APIProductCategoriesController::class)->except(['create', 'edit', 'destroy']);
+    Route::resource('products', APIProductsController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy']);
+    Route::resource('product-categories', APIProductCategoriesController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy']);
     Route::put('product-categories-sync', [APIProductCategoriesController::class, 'sync']);
 
     // Services
-    Route::resource('services', APIServicesController::class)->except(['create', 'edit', 'destroy']);
-    Route::resource('service-categories', APIServiceCategoriesController::class)->except(['create', 'edit', 'destroy']);
+    Route::resource('services', APIServicesController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy']);
+    Route::resource('service-categories', APIServiceCategoriesController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy']);
     Route::put('service-categories-sync', [APIServiceCategoriesController::class, 'sync']);
 
     // Invoices
-    Route::resource('invoices', APIInvoicesController::class)->except(['create', 'edit', 'destroy']);
+    Route::resource('invoices', APIInvoicesController::class, ['as' => 'api'])->except(['create', 'edit', 'destroy']);
     Route::put('invoice-void/{invoice}', [APIInvoicesController::class, 'void']);
     Route::put('invoice-restore/{invoice}', [APIInvoicesController::class, 'restore']);
     Route::put('invoice-send-reminder-notifications/{invoice}', [APIInvoicesController::class, 'sendReminderNotifications']);
